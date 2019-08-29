5a4b2bdb1d9f9f8b92bc724a9b2b2a03738f8ef5
Arthur Moreno <morenoarthur52@gmail.com>
2019-08-29 00:05:16 -0300

    Update

--- src/application/Program.java
+++ src/application/Program.java
@@ -39,6 +39,15 @@ public class Program {
 		sellerdao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
 		
+        System.out.println("\n==== TEST 5: seller Update ====");
+		
+		seller = sellerdao.findById(1);
+		seller.setName("gian freires");
+		seller.setEmail("gianfreires@gmail.com");
+		sellerdao.update(seller);
+		System.out.println("Update completed !!! ");
+		
+		
 
 	}
 
