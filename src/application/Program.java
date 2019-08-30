216fe10dbd5458783d8f2ac27a9261223493d746
Arthur Moreno <morenoarthur52@gmail.com>
2019-08-29 23:48:21 -0300

    Delete

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,6 +13,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		Scanner scan = new Scanner(System.in);
 		SellerDao sellerdao = DaoFactory.createSellerDao();
 		
 		System.out.println("==== TEST 1: seller findById ====");
@@ -47,6 +49,12 @@ public class Program {
 		sellerdao.update(seller);
 		System.out.println("Update completed !!! ");
 		
+		System.out.println("\n==== TEST 6: seller Delete ====");
+		
+		System.out.println("Enter id for delete teste: ");
+		int id = scan.nextInt();
+		sellerdao.deleteById(id);
+		System.out.println("Delete completed");
 		
 
 	}
